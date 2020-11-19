378b81feb518ba97381c950a4a8bd5daa5f2bacd
ElenaGrinberg <elena.grinberg@gmail.com>
2020-11-19 14:54:05 +0200

    add logger to FileManager

--- src/main/java/pages/FileManager.java
+++ src/main/java/pages/FileManager.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebElement;
 
 import java.io.File;
@@ -9,6 +11,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FileManager {
+    public static final Logger logger = LogManager.getLogger(FileManager.class);
+
 
     public static void createNewFile(List<WebElement> cars, String nameOfFile) {
 
@@ -16,7 +20,9 @@ public class FileManager {
         try {
             file.createNewFile();
         } catch (IOException e) {
+            logger.info("create file error");
             e.printStackTrace();
+
         }
         try {
             FileWriter fileWriter = new FileWriter(nameOfFile + ".txt");
@@ -27,6 +33,7 @@ public class FileManager {
 
         } catch (IOException e) {
             e.printStackTrace();
+            logger.info("file writer error");
 
         }
     }
